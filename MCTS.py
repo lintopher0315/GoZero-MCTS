38bd286a25c6dcd50d87eaa12bd1c40cd400b3a4
lintopher0315 <lin978@purdue.edu>
2020-02-23 18:16:32 -0500

    filled in simulation and backpropagation functions

--- MCTS.py
+++ MCTS.py
@@ -1,12 +1,13 @@
 import time
 import copy
+import random
 from tree import Tree
 from tree import Node
 
 class MCTS:
 
-    def find_next_move(self, board):
-        tree = Tree(board)
+    def find_next_move(self, board, player):
+        tree = Tree(board, player)
         root = tree.root
 
         timeout = time.time()+10
@@ -23,11 +24,10 @@ class MCTS:
             if len(explore_node.children) > 0:
                 explore_node = end_node.get_random_child()
 
-            #simulate
+            playout = self.simulate(explore_node)
+            self.backpropagate(explore_node, playout)
 
-            #backpropagate
-
-        return 0
+        return root.get_highest_child.board
 
     def select(self, node):
         selected = node
@@ -38,18 +38,40 @@ class MCTS:
     def expand(self, node):
         for i in range(19):
             for j in range(19):
-                if not node.board.invalid_inter(j, i, 1):
+                if not node.board.invalid_inter(j, i, node.player):
                     new_board = copy.deepcopy(node.board)
-                    new_board.update_board(j, i, 1)
-                    n = Node(new_board, node)
+                    new_board.update_board(j, i, node.player)
+                    n = Node(new_board, node, 3-node.player)
                     node.add_child(n)
         new_board = copy.deepcopy(node.board)
         new_board.passes += 1
-        n = Node(new_board, node)
+        n = Node(new_board, node, 3-node.player)
         node.add_child(n)
 
-    def simulate(self):
-        return 0
+    def simulate(self, node):
+        curr_node = node
+        while curr_node.board.has_neutral_territory():
+            possible_moves = []
+            for i in range(19):
+                for j in range(19):
+                    if not curr_node.board.invalid_inter(j, i, curr_node.player):
+                        possible_moves.append([j, i])
+            rand_move = random.choice(possible_moves)
+            new_board = copy.deepcopy(curr_node.board)
+            new_board.update_board(rand_move[0], rand_move[1], curr_node.player)
+            curr_node = Node(new_board, curr_node, 3-curr_node.player)
+        result = curr_node.board.get_score()
+        if result[0] > result[1]:
+            return 1
+        elif result[1] > result[0]:
+            return 2
+        else:
+            return 3
 
-    def backpropagate(self):
-        return 0
+    def backpropagate(self, node, result):
+        temp = node
+        while temp != None:
+            temp.visit_count += 1
+            if temp.player == result:
+                temp.score += 10
+            temp = temp.parent
--- tree.py
+++ tree.py
@@ -4,18 +4,28 @@ import math
 
 class Node:
 
-    def __init__(self, board, parent):
+    def __init__(self, board, parent, player):
         self.board = board
         self.children = []
         self.parent = parent
         self.visit_count = 0
         self.score = 0
+        self.player = player
     
     def add_child(self, node):
         self.children.append(node)
 
     def get_random_child(self):
         return random.choice(self.children)
+
+    def get_highest_child(self):
+        val = 0
+        index = 0
+        for i in range(len(self.children)):
+            if self.children[i].score > val:
+                val = self.children[i].score
+                index = i
+        return self.children[index]
 
     def ucb_value(self, parent_visit, win_score, visit):
         if visit == 0:
@@ -34,5 +44,5 @@ class Node:
     
 class Tree:
 
-    def __init__(self, board):
-        self.root = Node(board, None)
+    def __init__(self, board, player):
+        self.root = Node(board, None, player)
